771fe442d95bf97401ee361499502e1c31ab8511
eityeily <hotgold0905@gmail.com>
2024-06-19 05:10:55 +0900

    Add BSC Test network setting

--- src/constant/onchain/index.ts
+++ src/constant/onchain/index.ts
@@ -15,12 +15,16 @@ interface ConfigType {
 const ONCHAIN_CONFIG: ConfigType = {
   CHAIN_TO_RPC: {
     56: "https://go.getblock.io/67b1ef3e22b242f7a339387c8257063f",
+    97: "https://bsc-testnet-rpc.publicnode.com",
     137: "https://polygon-mainnet.infura.io/v3/254f726a173649a896fe0d63ab2085b9",
     534352: "https://scroll.blockpi.network/v1/rpc/public",
   },
   CHAIN_TO_ADDRESS: {
     56: {
       ZNS_REGISTRY: "0x7e2CF06F092c9F5cF5972eF021635b6c8E1C5bb2",
+    },
+    97: {
+      ZNS_REGISTRY: "0x91FdD937dd84493c168217bBE5d13B08B0bC2e6D",
     },
     137: {
       ZNS_REGISTRY: "0xBD32A89A76aFAFDDD8Fd514a292326377B6e4Cc9",
